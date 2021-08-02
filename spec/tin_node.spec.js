c541a51e2736630911b65da5d3d956b187adecfa
Kohei Otsuka <kochizufan@gmail.com>
2021-08-03 04:54:55 +0900

    Fix test script

--- spec/tin_node.spec.js
+++ spec/tin_node.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const Tin = require('../es5').default;
 const { toBeDeepCloseTo } = require("jest-matcher-deep-close-to");
 expect.extend({ toBeDeepCloseTo });
